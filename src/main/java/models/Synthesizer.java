31a4353c4adc05aec49efb1172134068cbd3d449
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-24 00:07:53 +1200

    Add Proccesses to models.Synthesizer

--- src/main/java/models/Synthesizer.java
+++ src/main/java/models/Synthesizer.java
@@ -1,15 +1,30 @@
 package models;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Synthesizer {
 
+    // TODO - Add Syntheiszer options - enums?
+
     public void preview(String text) {
+        ProcessBuilder processBuilder = new ProcessBuilder("espeak", text);
+        try {
+            Process process = processBuilder.start();
+        } catch (IOException e) {
+            // TODO - Error checking
+        }
         // Process - play audibly
         // Throws exceptions?
     }
 
     public void save(String text, File audioFile) {
+        ProcessBuilder processBuilder = new ProcessBuilder("espeak", "-w", audioFile.getPath() ,text);
+        try {
+            Process process = processBuilder.start();
+        } catch (IOException e) {
+            // TODO Error checking
+        }
         // Process - save text to audioFile
         // Throws exceptions?
     }
