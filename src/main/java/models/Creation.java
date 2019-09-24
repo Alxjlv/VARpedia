ecb1e734693aefc078b7925f89fdd7092f30ec96
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-25 00:43:38 +1200

    Add TODO's to models.Creation

--- src/main/java/models/Creation.java
+++ src/main/java/models/Creation.java
@@ -2,9 +2,12 @@ package models;
 
 import java.io.File;
 
-public class Creation {
+public class Creation { // TODO - Make serializable?
     private String name;
-    private File videoFile;
+    private File videoFile; // TODO - Add Media
+    // TODO - Add chunks
+    // TODO - Add images
+    // TODO - Add creation time?
 
     public Creation(String name, File videoFile) {
         this.name = name;
