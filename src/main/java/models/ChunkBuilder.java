1f2ff6b441e22a7213aa14c31b6b7399ca7f08ac
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-25 01:01:10 +1200

    Add comments for models.ChunkBuilder

--- src/main/java/models/ChunkBuilder.java
+++ src/main/java/models/ChunkBuilder.java
@@ -2,23 +2,40 @@ package models;
 
 import java.io.File;
 
+/**
+ * Implements a {@link Builder} for {@link Chunk} objects
+ */
 public class ChunkBuilder implements Builder<Chunk> {
     private int id;
     private String text;
 //    private File audioFile; // TODO - Not needed as field? Only used in build()
     private Synthesizer synthesizer;
 
-    // Package Private
+    /**
+     * Set the id for the chunk to be built. This method is package-private. Only intended to be called by {@link Manager}
+     * @param id The id for the chunk to be built
+     * @return {@code this}
+     */
     ChunkBuilder setId(int id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * Set the text to be spoken for the chunk to be built
+     * @param text The text to be spoken
+     * @return {@code this}
+     */
     public ChunkBuilder setText(String text) {
         this.text = text;
         return this;
     }
 
+    /**
+     * Set the {@link Synthesizer} for the chunk to be built
+     * @param synthesizer The Synthesizer for the chunk
+     * @return {@code this}
+     */
     public ChunkBuilder setSynthesizer(Synthesizer synthesizer) {
         this.synthesizer = synthesizer;
         return this;
