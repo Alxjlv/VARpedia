8ba59f444e6b131fba47e6df83db0632d4b65cc1
Alxjlv <alxjlv@gmail.com>
2019-09-24 14:53:16 +1200

    Attempting to parse xml from an http request

--- src/main/java/main/Search.java
+++ src/main/java/main/Search.java
@@ -1,59 +1,55 @@
 package main;
 
-import com.flickr4java.flickr.Flickr;
-import com.flickr4java.flickr.FlickrException;
-import com.flickr4java.flickr.REST;
-import com.flickr4java.flickr.RequestContext;
-import com.flickr4java.flickr.photos.Photo;
-import com.flickr4java.flickr.photos.PhotoList;
-import com.flickr4java.flickr.photos.PhotosInterface;
-import com.flickr4java.flickr.photos.SearchParameters;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import javafx.concurrent.Task;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Search {
 
-//    private String _apiKey = Keys.FLICKR_PUBLIC;
-//    private String _apiSecret = Keys.FLICKR_SECRET;
-//
-//    private Flickr flickr;
-//    private REST rest;
-//    private RequestContext requestContext;
-//
-//    public Search(){
-//        flickr = new Flickr(_apiKey,_apiSecret, new REST());
-//        requestContext = RequestContext.getRequestContext();
-//        Flickr.debugRequest = false;
-//        Flickr.debugStream = false;
-//    }
-//
-//    public void search(String search){
-//        PhotosInterface photos = flickr.getPhotosInterface();
-//        SearchParameters params = new SearchParameters();
-//        params.setSort(6);
-//        params.setText(search);
-//        try{
-//            PhotoList<Photo> results = photos.search(params,5,0);
-//            for(Photo p : results){
-//                System.out.println(String.format("Title: %s",p.getTitle()));
-//                System.out.println("Media: " + p.getMedia());
-//                System.out.println("Original URL: " + p.getOriginalUrl());
-//            }
-//        }catch (FlickrException f){
-//
-//
-//        }
-//
-//
-//
-//    }
-    try{
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        
+    public List<String> Search(String search, int num){
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
+                "&api_key="+Keys.FLICKR_PUBLIC+
+                "&text="+search+
+                "&per_page="+num +
+                "&sort=relevance";
+        Request request = new Request.Builder().url(url).build();
+
+        ExecutorService thread = Executors.newSingleThreadExecutor();
+        Task<Response> call = new Task<Response>() {
+            @Override
+            protected Response call() throws Exception {
+                try{
+                    Response response = client.newCall(request).execute();
+                    return response;
+                }catch(IOException i){
+                    i.printStackTrace();
+                }
+                return null;
+            }
+        };
+        thread.submit(call);
+        call.setOnSucceeded(event -> {
+            Response r = call.getValue();
+            try {
+                String XMLString = r.body().string();
+                XMLParser parser = new XMLParser();
+                parser.parse(XMLString);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        return null;
     }
+
+
 
 }
---
+++ src/main/java/main/XMLParser.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.List;
+
+public class XMLParser {
+
+    public List<String> parse(String XMLString){
+
+        return null;
+    }
+
+}
