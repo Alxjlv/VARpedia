0b41ed9d8d5a266ab79769811572aaf855d13a8e
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-25 00:53:34 +1200

    Add comment for models.Builder

--- src/main/java/models/Builder.java
+++ src/main/java/models/Builder.java
@@ -1,5 +1,9 @@
 package models;
 
+/**
+ * Specifies an interface for {@link Manager} to build objects
+ * @param <T> The type of object to be built
+ */
 public interface Builder<T> {
     T build();
 }
