0a0d43f49d3b2f4c38f38c7aff974fd446c41230
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-27 14:59:32 +1200

    Upgrade video player

--- src/main/java/controllers/VideoView.java
+++ src/main/java/controllers/VideoView.java
@@ -1,48 +1,211 @@
 package controllers;
 
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 import models.Creation;
 import models.MediaSingleton;
 
+/**
+ * Code based on: https://docs.oracle.com/javafx/2/media/playercontrol.htm
+ */
 public class VideoView extends Controller {
 
-    @FXML MediaView mediaView;
+    @FXML
+    private MediaView mediaView;
 
     @FXML
-    Text creationName;
+    private Button playButton;
     @FXML
-    Text elapsedTime;
+    private ToggleButton muteButton;
     @FXML
-    Text totalTime;
+    private Slider timeSlider;
+
+    @FXML
+    private Text creationName;
+    @FXML
+    private Text elapsedTime;
+    @FXML
+    private Text totalTime;
 
     private Media media;
     private MediaPlayer mediaPlayer;
+
+    private Duration duration;
+    private boolean atEndOfMedia = false;
+    private boolean stopRequested = false;
 
     @FXML
     public void initialize() {
         Creation creation = MediaSingleton.getInstance().getCreation();
 
-        creationName.setText(creation.getName());
-
         media = new Media(creation.getVideoFile().toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
+
+        playButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Press play");
+                MediaPlayer.Status status = mediaPlayer.getStatus();
+
+                if (status == MediaPlayer.Status.UNKNOWN
+                        || status == MediaPlayer.Status.HALTED) {
+                    return;
+                }
+
+                if (status == MediaPlayer.Status.PAUSED
+                        || status == MediaPlayer.Status.READY
+                        || status == MediaPlayer.Status.STOPPED) {
+                    if (atEndOfMedia) {
+                        mediaPlayer.seek(mediaPlayer.getStartTime());
+                        atEndOfMedia = false;
+                    }
+                    mediaPlayer.play();
+                } else {
+                    mediaPlayer.pause();
+                }
+            }
+        });
+
+        muteButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Press mute");
+                mediaPlayer.setMute(!mediaPlayer.isMute());
+            }
+        });
+
+        mediaPlayer.currentTimeProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                System.out.println("Update time");
+                updateValues();
+            }
+        });
+
+        mediaPlayer.setOnPlaying(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Player playing");
+                if (stopRequested) {
+                    mediaPlayer.pause();
+                    stopRequested = false;
+                } else {
+                    playButton.setText("||");
+                }
+            }
+        });
+
+        mediaPlayer.setOnPaused(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Player paused");
+                playButton.setText("|>");
+            }
+        });
+
+        mediaPlayer.setOnReady(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Player ready");
+
+                duration = mediaPlayer.getMedia().getDuration();
+
+                if (totalTime != null) {
+                    totalTime.setText(formatTime(duration));
+                    System.out.println(duration.toString()); // TODO - Remove
+                }
+
+                updateValues();
+            }
+        });
+
+        mediaPlayer.setOnEndOfMedia(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("End of media");
+                System.out.println(mediaPlayer.getCurrentTime().toString());
+                playButton.setText("|>");
+                stopRequested = true;
+                atEndOfMedia = true;
+                updateValues();
+            }
+        });
+
+//        timeSlider.valueProperty().addListener(new InvalidationListener() {
+//            @Override
+//            public void invalidated(Observable observable) {
+//                System.out.println("Changing time slider");
+//                if (timeSlider.isValueChanging()) {
+//                    System.out.println("Seek to: "+duration.multiply(timeSlider.getValue() / 100.0).toString());
+//                    mediaPlayer.seek(duration.multiply(timeSlider.getValue() / 100.0));
+//                }
+//            }
+//        });
+        timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                System.out.println("Time slider changed: "+observable+" Old value: "+oldValue+" New value: "+newValue);
+                System.out.println("Is changing:"+timeSlider.isValueChanging());
+            }
+        });
+
+        creationName.setText(creation.getName());
 
         elapsedTime.setText(mediaPlayer.getCurrentTime().toString());
         totalTime.setText(media.getDuration().toString());
     }
 
-    // TODO - Finalise time implementation
-    // TODO - Add mute/volume controls
-    // TODO - Implement slider control for scrubbing
+    protected void updateValues() {
+        if (elapsedTime != null && timeSlider != null && muteButton != null) {
+            Platform.runLater(new Runnable() {
+                public void run() {
+                    Duration currentTime = mediaPlayer.getCurrentTime();
 
-    @FXML
-    public void pressPlay() {
-        mediaPlayer.play();
-        // TODO - Improve play controls: Click on video to Play/Pause. Looping. Press spacebar?
+                    elapsedTime.setText(formatTime(currentTime));
+                    System.out.println(currentTime.toString()); // TODO - Remove
+
+                    timeSlider.setDisable(duration.isUnknown());
+                    if (!timeSlider.isDisabled()
+                            && duration.greaterThan(Duration.ZERO)
+                            && !timeSlider.isValueChanging()) {
+                        timeSlider.adjustValue(currentTime.divide(duration.toMillis()).toMillis() * 100);
+                    }
+
+                    // TODO - Make muteButton ToggleButton
+                    // muteButton;4
+                }
+            });
+        }
+    }
+
+    private static String formatTime(Duration time) {
+        int intTime = (int) Math.floor(time.toSeconds());
+        int timeHours = intTime / (60 * 60);
+        if (timeHours > 0) {
+            intTime -= timeHours * 60 * 60;
+        }
+        int timeMinutes = intTime / 60;
+        int timeSeconds = intTime - timeHours * 60 * 60 - timeMinutes * 60;
+
+        if (timeHours > 0) {
+            return String.format("%d:%02d:%02d", timeHours, timeMinutes, timeSeconds);
+        } else {
+            return String.format("%02d:%02d", timeMinutes, timeSeconds);
+        }
     }
 }
