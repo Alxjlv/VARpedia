ce9f71efbc9455c03171698f7cac666d3dbe1f09
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-29 14:19:59 +1300

    Add Warning Alert if user selects more than 40 words to synthesize

--- src/main/java/controllers/ChunkView.java
+++ src/main/java/controllers/ChunkView.java
@@ -22,6 +22,7 @@ import views.ChunkCellFactory;
 
 import java.io.File;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class ChunkView extends Controller {
 
@@ -163,23 +164,37 @@ public class ChunkView extends Controller {
     }
 
     @FXML public void pressPreview() {
-        // TODO - Word count validation (20-40)
-        // TODO - add ability to stop preview (e.g. preview button becomes cancel/stop button)
-        synthesizer.preview(searchResult.getSelectedText());
+        if (checkWords(searchResult.getSelectedText())) {
+            // TODO - Word count validation (20-40)
+            // TODO - add ability to stop preview (e.g. preview button becomes cancel/stop button)
+            synthesizer.preview(searchResult.getSelectedText());
+        }
     }
 
     @FXML public void pressSaveSnippet() {
-        // TODO - Word count validation (20-40)
-        ChunkBuilder chunkBuilder = ChunkManager.getInstance().getBuilder();
-        chunkBuilder.setText(searchResult.getSelectedText()).setSynthesizer(synthesizer);
-        ChunkManager.getInstance().create(chunkBuilder);
+        if (checkWords(searchResult.getSelectedText())) {
+            // TODO - Word count validation (20-40)
+            ChunkBuilder chunkBuilder = ChunkManager.getInstance().getBuilder();
+            chunkBuilder.setText(searchResult.getSelectedText()).setSynthesizer(synthesizer);
+            ChunkManager.getInstance().create(chunkBuilder);
+        }
+    }
+
+    private boolean checkWords(String string) {
+        StringTokenizer tokenizer = new StringTokenizer(string);
+        if (tokenizer.countTokens() > 40) {
+            System.out.println("Popup: more than 40 words");
+
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Please select less than 40 words to synthesize text");
+            alert.showAndWait();
+
+            return false;
+        }
+        return true;
     }
 
     @FXML public void pressPlayback() {
-        // TODO - Turn this into Task and make synthesizer.preview() block while running allowing sequential (not simultaneous) playback
-        Iterator<Chunk> iterator = chunksListView.getItems().iterator();
-
-        recursivePlayback(iterator);
+        recursivePlayback(chunksListView.getItems().iterator());
     }
 
     private void recursivePlayback(Iterator<Chunk> iterator) {
@@ -193,14 +208,6 @@ public class ChunkView extends Controller {
             Media media = new Media(audioFile.toURI().toString());
             MediaPlayer player = new MediaPlayer(media);
             player.setAutoPlay(true);
-
-//            player.onEndOfMediaProperty().addListener(new ChangeListener<Runnable>() {
-//                @Override
-//                public void changed(ObservableValue<? extends Runnable> observable, Runnable oldValue, Runnable newValue) {
-//                    System.out.println("Ended?");
-//                    recursivePlayback(iterator);
-//                }
-//            });
             player.setOnEndOfMedia(new Runnable() {
                 @Override
                 public void run() {
@@ -210,33 +217,14 @@ public class ChunkView extends Controller {
             });
         }
     }
-//        Task playback = new Task<Void>() {
-//            @Override
-//            protected Void call() throws Exception {
-//                for (Chunk chunk: chunksListView.getItems()) {
-//                    chunksListView.getSelectionModel().select(chunk);
-//                    synthesizer.preview(chunk.getText());
-//                }
-//
-//                return null;
-//            }
-//        }
-
 
     @FXML public void pressPreviewSnippet() {
         // TODO - add ability to stop preview (e.g. preview button becomes cancel/stop button, click on a different snippet)
-//        synthesizer.preview(chunksListView.getSelectionModel().selectedItemProperty().getValue().getText());
         File audioFile = new File(chunksListView.getSelectionModel().getSelectedItem().getFolder(), "audio.wav");
 
         Media media = new Media(audioFile.toURI().toString());
         MediaPlayer player = new MediaPlayer(media);
         player.setAutoPlay(true);
-//        player.onReadyProperty().addListener(new ChangeListener<Runnable>() {
-//            @Override
-//            public void changed(ObservableValue<? extends Runnable> observable, Runnable oldValue, Runnable newValue) {
-//                player.play();
-//            }
-//        });
     }
 
     @FXML public void pressDelete() {
