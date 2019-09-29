5d9c51ec092e8726d31abd490811a179b1e1c512
Tait Fuller <32445605+taitfuller@users.noreply.github.com>
2019-09-29 21:45:34 +1300

    Implement Tait's code from A2

--- src/main/java/models/CreationManager.java
+++ src/main/java/models/CreationManager.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.Comparator;
 
 /**
@@ -12,7 +13,28 @@ import java.util.Comparator;
 public class CreationManager extends Manager<Creation> {
     private static CreationManager instance;
 
+    private final File creationsFolder;
+
     private CreationManager() {
+        creationsFolder = new File("creations/");
+        items = FXCollections.observableArrayList();
+
+        if (creationsFolder.exists()) { // TODO Concurrency?
+            File[] creationFolders = creationsFolder.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.isDirectory();
+                }
+            });
+            for (File creationFolder: creationFolders) {
+                File videoFile = new File(creationFolder, "video.mp4");
+                if (videoFile.exists()) {
+                    items.add(new Creation(creationFolder.getName(), creationFolder));
+                }
+            }
+        } else {
+            creationsFolder.mkdir();
+        }
     }
 
     /**
