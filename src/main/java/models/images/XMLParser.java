970144838f73ad593a6be7b605a4cf073d0007e9
Alex <alxjlv@gmail.com>
2019-10-10 21:11:58 +1300

    Commented on XMLParser

--- src/main/java/models/images/XMLParser.java
+++ src/main/java/models/images/XMLParser.java
@@ -9,7 +9,6 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.soap.Node;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
@@ -18,22 +17,36 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * XMLParser is used to process the response XML from flickr and return the URLs and ID's of the images that are returned
+ */
 class XMLParser {
 
+    /**
+     * This is the main method used to retrieve the relevant information from the XML document. This information
+     * includes the URL of the image, and the image id.
+     * @param XMLString - this is the string returned from the Http request to flickr
+     * @return - null if the response was blank, a HashMap with URL as the key and the id as the value otherwise
+     * @throws MalformedURLException - if the URL isn't valid
+     */
     Map<URL,File> parse(String XMLString) throws MalformedURLException {
         File imageFolder = new File(".images/");
-        Document doc = convertToXML(XMLString);
+        Document doc = convertToXML(XMLString);//Converting to parse-able XML
         Map<URL,File> urlList = new HashMap<>();
         if (doc != null) {
+            //Traversing the XML document to get to the correct element
             NodeList photos = doc.getElementsByTagName("photos");
             Element photosElement = (Element) photos.item(0);
             NodeList list = photosElement.getElementsByTagName("photo");
+            //Cycling through each photo item from the response
             for(int i=0;i<list.getLength();i++){
                 String id = list.item(i).getAttributes().getNamedItem("id").getTextContent();
                 File image = new File(imageFolder,id+".jpg");
+                //Sometimes the URL field can be blank so we need to check for that
                 if(list.item(i).getAttributes().getNamedItem("url_m")!=null){
                     String link = list.item(i).getAttributes().getNamedItem("url_m").getTextContent();
                     System.out.println("link number: " + i + " " +link);
+                    //Adding the URL and image file to the HashMap
                     URL url = new URL(link);
                     urlList.put(url,image);
                 }
@@ -43,6 +56,11 @@ class XMLParser {
         return null;
     }
 
+    /**
+     * This method is used to convert a String XML object into a parse-able XML document
+     * @param XMLString - A String containing XML
+     * @return - if successful this will return a parse-able Document object
+     */
     private Document convertToXML(String XMLString){
         try{
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
