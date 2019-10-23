38536ef7427aa93764edfffde213a891e1b0cb72
Alex <alxjlv@gmail.com>
2019-10-24 11:10:41 +1300

    Fixed formanager to reset number of images between creations

--- src/main/java/models/FormManager.java
+++ src/main/java/models/FormManager.java
@@ -66,6 +66,7 @@ public class FormManager {
         instance.thumbnail.set(null);
         instance.name.set(null);
         instance.backgroundMusic.set(null);
+        instance.numberOfImages.set(null);
     }
 
     public State getState() {
